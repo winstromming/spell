e8a3f37658bac65db7940b52a5f529e5282f88a4
Jesper Winström <jay@nopr.co>
2022-11-12 16:12:58 +0100

    feature: multiple characters

--- code/components/Main.vue
+++ code/components/Main.vue
@@ -43,7 +43,7 @@
                         <Trash />
                       </n-icon>
                     </template>
-                    Delete
+                    Delete this character
                   </n-button>
                 </n-space>
               </template>
